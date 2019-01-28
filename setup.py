ce5b4721da3406fce250872bf7aa235013965ad8
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2019-01-28 05:28:31 -0500

    setup: use scikit-build for building C module
    scikit-build is a drop-in replacement to setuptools.setup function
    allowing to easily compile and package extensions (C/C++/Cython) by
    bridging CMake and setuptools. See http://scikit-build.org
    
    CMake is an open-source, cross-platform family of tools designed
    to build, test and package software. See https://cmake.org
    
    Ninja is a small build system with a focus on speed. It differs from other
    build systems in two major respects: it is designed to have its input
    files generated by a higher-level build system, and it is designed
    to run builds as fast as possible. See https://ninja-build.org/
    
    By adding the "pyproject.toml" file, we ensure the required packages
    are installed when running "pip wheel . -w dist".
    
    To streamline testing and creation of source or built distribution, the
    "requirements_dev.txt" was also updated to include scikit-build, cmake
    and ninja.
    
    This commit introduces new build dependencies all available as wheels:
    scikit-built, cmake and ninja.

--- setup.py
+++ setup.py
@@ -12,19 +12,21 @@
 from __future__ import print_function
 
 import os
-import platform
-import re
 import sys
 
-from setuptools import Extension
-from setuptools import setup
-from glob import glob
+from skbuild import setup
+
+# Workaround to ensure cpuinfo can be imported using latest pip.
+# For more details, see https://github.com/pypa/pip/issues/6163
+sys.path.insert(0, os.path.dirname(__file__))
 import cpuinfo
 
-########### Check versions ##########
+
+# ########## Check versions ##########
 def exit_with_error(message):
     print('ERROR: %s' % message)
     sys.exit(1)
+
 
 # Check for Python
 if sys.version_info[0] == 2:
@@ -38,7 +40,7 @@ else:
 
 tests_require = ['numpy']
 
-########### End of checks ##########
+# ########## End of checks ##########
 
 # Read the long_description from README.rst
 with open('README.rst') as f:
@@ -49,120 +51,23 @@ VERSION = open('VERSION').read().strip()
 # Create the version.py file
 open('blosc/version.py', 'w').write('__version__ = "%s"\n' % VERSION)
 
+
 # Global variables
-CFLAGS = os.environ.get('CFLAGS', '').split()
-LFLAGS = os.environ.get('LFLAGS', '').split()
-# Allow setting the Blosc dir if installed in the system
-BLOSC_DIR = os.environ.get('BLOSC_DIR', '')
-
-# Check for USE_CODEC environment variables
-try:
-    INCLUDE_LZ4 = os.environ['INCLUDE_LZ4'] == '1'
-except KeyError:
-    INCLUDE_LZ4 = True
-try:
-    INCLUDE_SNAPPY = os.environ['INCLUDE_SNAPPY'] == '1'
-except KeyError:
-    INCLUDE_SNAPPY = False  # Snappy is disabled by default
-try:
-    INCLUDE_ZLIB = os.environ['INCLUDE_ZLIB'] == '1'
-except KeyError:
-    INCLUDE_ZLIB = True
-try:
-    INCLUDE_ZSTD = os.environ['INCLUDE_ZSTD'] == '1'
-except KeyError:
-    INCLUDE_ZSTD = True
-
+# CFLAGS = os.environ.get('CFLAGS', '').split()
+# LFLAGS = os.environ.get('LFLAGS', '').split()
 
 # Handle --blosc=[PATH] --lflags=[FLAGS] --cflags=[FLAGS]
-args = sys.argv[:]
-for arg in args:
-    if arg.find('--blosc=') == 0:
-        BLOSC_DIR = os.path.expanduser(arg.split('=')[1])
-        sys.argv.remove(arg)
-    if arg.find('--lflags=') == 0:
-        LFLAGS = arg.split('=')[1].split()
-        sys.argv.remove(arg)
-    if arg.find('--cflags=') == 0:
-        CFLAGS = arg.split('=')[1].split()
-        sys.argv.remove(arg)
-
-
-# Blosc sources and headers
-
-# To avoid potential namespace collisions use build_clib.py for each codec
-# instead of co-compiling all sources files in one setuptools.Extension object.
-clibs = [] # for build_clib, libraries TO BE BUILT
-
-# Below are parameters for the Extension object
-sources = ["blosc/blosc_extension.c"]
-inc_dirs = []
-lib_dirs = []
-libs = []  # Pre-built libraries ONLY, like python36.so
-def_macros = []
-if BLOSC_DIR != '':
-    # Using the Blosc library
-    lib_dirs += [os.path.join(BLOSC_DIR, 'lib')]
-    inc_dirs += [os.path.join(BLOSC_DIR, 'include')]
-    libs += ['blosc']
-else:
-
-    # Configure the Extension
-    # Compiling everything from included C-Blosc sources
-    sources += [f for f in glob('c-blosc/blosc/*.c')
-                if 'avx2' not in f and 'sse2' not in f]
-
-    inc_dirs += [os.path.join('c-blosc', 'blosc')]
-    inc_dirs += glob('c-blosc/internal-complibs/*')
-
-    # Codecs to be built with build_clib
-    if INCLUDE_LZ4:
-        clibs.append( ('lz4', {'sources': glob('c-blosc/internal-complibs/lz4*/*.c')} ) )
-        inc_dirs += glob('c-blosc/internal-complibs/lz4*')
-        def_macros += [('HAVE_LZ4',1)]
-
-    # Tried and failed to compile Snappy with gcc using 'cflags' on posix
-    # setuptools always uses gcc instead of g++, as it only checks for the 
-    # env var 'CC' and not 'CXX'.
-    if INCLUDE_SNAPPY:
-        clibs.append( ('snappy', {'sources': glob('c-blosc/internal-complibs/snappy*/*.cc'), 
-                                  'cflags': ['-std=c++11', '-lstdc++'] } ) )
-        inc_dirs += glob('c-blosc/internal-complibs/snappy*')
-        def_macros += [('HAVE_SNAPPY',1)]
-
-    if INCLUDE_ZLIB:
-        clibs.append( ('zlib', {'sources': glob('c-blosc/internal-complibs/zlib*/*.c')} ) )
-        def_macros += [('HAVE_ZLIB',1)]
-
-    if INCLUDE_ZSTD:
-        clibs.append( ('zstd', {'sources': glob('c-blosc/internal-complibs/zstd*/*/*.c'), 
-                    'include_dirs': glob('c-blosc/internal-complibs/zstd*') + glob('c-blosc/internal-complibs/zstd*/common') } ) )
-        inc_dirs += glob('c-blosc/internal-complibs/zstd*/common')
-        inc_dirs += glob('c-blosc/internal-complibs/zstd*')
-        def_macros += [('HAVE_ZSTD',1)]
-    
-
-    # Guess SSE2 or AVX2 capabilities
-    cpu_info = cpuinfo.get_cpu_info()
-    # SSE2
-    if 'DISABLE_BLOSC_SSE2' not in os.environ and (cpu_info != None) and ('sse2' in cpu_info['flags']):
-        print('SSE2 detected')
-        CFLAGS.append('-DSHUFFLE_SSE2_ENABLED')
-        sources += [f for f in glob('c-blosc/blosc/*.c') if 'sse2' in f]
-        if os.name == 'posix':
-            CFLAGS.append('-msse2')
-        elif os.name == 'nt':
-            def_macros += [('__SSE2__', 1)]
-    # AVX2
-    if 'DISABLE_BLOSC_AVX2' not in os.environ and (cpu_info != None) and ('avx2' in cpu_info['flags']):
-        print('AVX2 detected')
-        CFLAGS.append('-DSHUFFLE_AVX2_ENABLED')
-        sources += [f for f in glob('c-blosc/blosc/*.c') if 'avx2' in f]
-        if os.name == 'posix':
-            CFLAGS.append('-mavx2')
-        elif os.name == 'nt':
-            def_macros += [('__AVX2__', 1)]
-    # TODO: AVX512
+# args = sys.argv[:]
+# for arg in args:
+#     if arg.find('--blosc=') == 0:
+#         BLOSC_DIR = os.path.expanduser(arg.split('=')[1])
+#         sys.argv.remove(arg)
+#     if arg.find('--lflags=') == 0:
+#         LFLAGS = arg.split('=')[1].split()
+#         sys.argv.remove(arg)
+#     if arg.find('--cflags=') == 0:
+#         CFLAGS = arg.split('=')[1].split()
+#         sys.argv.remove(arg)
 
 classifiers = """\
 Development Status :: 5 - Production/Stable
@@ -182,6 +87,13 @@ Operating System :: Microsoft :: Windows
 Operating System :: Unix
 """
 
+cpu_info = cpuinfo.get_cpu_info()
+
+
+def cmake_bool(cond):
+    return 'ON' if cond else 'OFF'
+
+
 setup(name = "blosc",
       version = VERSION,
       description = 'Blosc data compressor',
@@ -198,19 +110,16 @@ Blosc is a high performance compressor optimized for binary data.
       url = 'http://github.com/blosc/python-blosc',
       license = 'https://opensource.org/licenses/BSD-3-Clause',
       platforms = ['any'],
-      libraries = clibs,
-      ext_modules = [
-        Extension( "blosc.blosc_extension",
-                   include_dirs=inc_dirs,
-                   define_macros=def_macros,
-                   sources=sources,
-                   library_dirs=lib_dirs,
-                   libraries=libs,
-                   extra_link_args=LFLAGS,
-                   extra_compile_args=CFLAGS ),
-        ],
+      cmake_args = [
+          '-DBLOSC_DIR:PATH=%s' % os.environ.get('BLOSC_DIR', ''),
+          '-DDEACTIVATE_SSE2:BOOL=%s' % cmake_bool('DISABLE_BLOSC_SSE2' in os.environ or 'sse2' not in cpu_info['flags']),
+          '-DDEACTIVATE_AVX2:BOOL=%s' % cmake_bool('DISABLE_BLOSC_AVX2' in os.environ),
+          '-DDEACTIVATE_LZ4:BOOL=%s' % cmake_bool(not int(os.environ.get('INCLUDE_LZ4', '1'))),
+          # Snappy is disabled by default
+          '-DDEACTIVATE_SNAPPY:BOOL=%s' % cmake_bool(not int(os.environ.get('INCLUDE_SNAPPY', '0'))),
+          '-DDEACTIVATE_ZLIB:BOOL=%s' % cmake_bool(not int(os.environ.get('INCLUDE_ZLIB', '1'))),
+          '-DDEACTIVATE_ZSTD:BOOL=%s' % cmake_bool(not int(os.environ.get('INCLUDE_ZSTD', '1'))),
+      ],
       tests_require=tests_require,
-      zip_safe=False,
       packages = ['blosc'],
-
 )
